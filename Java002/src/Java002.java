778b12bcd2aa1b19797a74171218e6e4640c5ed5
AndreaAragao <93750549+AndreaAragao@users.noreply.github.com>
2021-11-13 11:39:52 -0300

    Create Java002.java

---
+++ Java002/src/Java002.java
@@ -0,0 +1,15 @@
+
+public class Java002 {
+
+	public static void main(String[] args) {
+		int num1 = 111;
+		int num2 = 222;
+		int num3 = 333;
+		int num4 = 444;
+		int soma;
+		
+		soma = num1 + num2 + num3 + num4;
+		System.out.println(soma);
+	} 
+
+}
