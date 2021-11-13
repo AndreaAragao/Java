d3726ca92b0fe9fbf67a0d45df816a84ba4cea7a
AndreaAragao <93750549+AndreaAragao@users.noreply.github.com>
2021-11-13 11:33:46 -0300

    Create Java001.java

---
+++ Java001/src/Java001.java
@@ -0,0 +1,16 @@
+
+public class Java001 {
+
+	public static void main(String[] args) {
+
+		int num1 = 105;
+		int num2 = 105;
+		int num3 = 150;
+		int soma;
+
+		soma = num1 + num2 + num3;
+		System.out.println(soma);
+
+	}
+
+}
