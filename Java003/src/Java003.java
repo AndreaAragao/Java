cc6e62419937e092c705c515218b63d23f08204e
AndreaAragao <93750549+AndreaAragao@users.noreply.github.com>
2021-11-13 11:44:58 -0300

    Create Java003.java

---
+++ Java003/src/Java003.java
@@ -0,0 +1,18 @@
+// soma de 6 numeros
+public class Java003 {
+
+	public static void main(String[] args) {
+		int num1 = 123;
+		int num2 = 321;
+		int num3 = 456;
+		int num4 = 654;
+		int num5 = 789;
+		int num6 = 987;
+		int soma;
+
+		soma = num1 + num2 + num3 + num4 + num5 + num6;
+		System.out.println(soma);
+
+	}
+
+}
